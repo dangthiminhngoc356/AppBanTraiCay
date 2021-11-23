3ff355eed0ac71607fb08d6f28e1c9c4be8dcad0
LeHuuThang <bilun4450528@gmail.com>
2021-11-23 10:06:08 +0700

    Đăng nhập phân quyền

--- app/src/main/java/com/example/appbantraicay/model/Users.java
+++ app/src/main/java/com/example/appbantraicay/model/Users.java
@@ -1,16 +1,17 @@
 package com.example.appbantraicay.model;
 
 public class Users {
-    private String name,password,phone;
+    private String name, password, phone, role;
 
     public Users(){
 
     }
 
-    public Users(String name, String password, String phone) {
+    public Users(String name, String password, String phone, String role) {
         this.name = name;
         this.password = password;
         this.phone = phone;
+        this.role = role;
     }
 
     public String getName() {
@@ -35,5 +36,13 @@ public class Users {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
