eab109b47e2e9ff44a87a01ad567a29a690fe540
thanglehuu <57870045+thanglehuu@users.noreply.github.com>
2021-11-24 12:15:41 +0700

    Thang le (#16)
    * Đăng nhập phân quyền (#3)
    
    * Revert "Đăng nhập phân quyền (#3)" (#4)
    
    This reverts commit 0af64b5945de1b1b7d8f84bd383459c002596fc4.
    
    * admin , remember password (#5)
    
    * order user
    
    * Admin layout
    
    * order user (#9)
    
    * Quan ly nha phan phoi
    
    * Hoàn thành quản lý sản phẩm
    
    * Thang le (#11)
    
    * Hoàn thành quản lý sản phẩm
    
    * fix lỗi
    
    * fix lỗi
    
    * Thang le (#12)
    
    * Hoàn thành quản lý sản phẩm
    
    * đặng thị minh ngọc (#13) (#14)
    
    * Đăng nhập phân quyền (#3)
    
    * Revert "Đăng nhập phân quyền (#3)" (#4)
    
    This reverts commit 0af64b5945de1b1b7d8f84bd383459c002596fc4.
    
    * admin , remember password (#5)
    
    * order user
    
    * Admin layout
    
    * Quan ly nha phan phoi
    
    * Thang le (#11)
    
    * Hoàn thành quản lý sản phẩm
    
    * fix lỗi
    
    * Thang le (#12)
    
    * Hoàn thành quản lý sản phẩm
    
    Co-authored-by: dangthiminhngoc356 <81677658+dangthiminhngoc356@users.noreply.github.com>
    Co-authored-by: 197PM21988 - NGUYỄN HỒNG THỊNH - K25T-PM1 <thinh.197pm21988@vanlanguni.vn>
    Co-authored-by: thinhnguyen <81676598+nguyenthinh2210@users.noreply.github.com>
    Co-authored-by: 197PM31180 - BÙI HOÀNG NAM - K25T-PM1 <nam.197pm31180@vanlanguni.vn>
    Co-authored-by: buihoangnam <81286675+buihoangnam@users.noreply.github.com>
    
    Co-authored-by: dangthiminhngoc356 <81677658+dangthiminhngoc356@users.noreply.github.com>
    Co-authored-by: 197PM21988 - NGUYỄN HỒNG THỊNH - K25T-PM1 <thinh.197pm21988@vanlanguni.vn>
    Co-authored-by: thinhnguyen <81676598+nguyenthinh2210@users.noreply.github.com>
    Co-authored-by: 197PM31180 - BÙI HOÀNG NAM - K25T-PM1 <nam.197pm31180@vanlanguni.vn>
    Co-authored-by: buihoangnam <81286675+buihoangnam@users.noreply.github.com>
    
    Co-authored-by: dangthiminhngoc356 <81677658+dangthiminhngoc356@users.noreply.github.com>
    Co-authored-by: 197PM21988 - NGUYỄN HỒNG THỊNH - K25T-PM1 <thinh.197pm21988@vanlanguni.vn>
    Co-authored-by: thinhnguyen <81676598+nguyenthinh2210@users.noreply.github.com>
    Co-authored-by: 197PM31180 - BÙI HOÀNG NAM - K25T-PM1 <nam.197pm31180@vanlanguni.vn>
    Co-authored-by: buihoangnam <81286675+buihoangnam@users.noreply.github.com>

--- app/src/main/java/com/example/appbantraicay/AdminActivity.java
+++ app/src/main/java/com/example/appbantraicay/AdminActivity.java
@@ -13,12 +13,6 @@ public class AdminActivity extends AppCompatActivity {
     TextView tenadmin;
     Button sanpham, donhang, nhaphanphoi, loaisp, admindangxuat;
     String loggedinusername;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.Toast;
-
-public class AdminActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +50,6 @@ public class AdminActivity extends AppCompatActivity {
         loaisp = (Button) findViewById(R.id.btn_loaisp);
         tenadmin = (TextView) findViewById(R.id.txt_tenAdmin);
         tenadmin.setText(loggedinusername);
-      
-        Toast.makeText(this, "Welcome Admin", Toast.LENGTH_SHORT).show();
 
     }
 }
