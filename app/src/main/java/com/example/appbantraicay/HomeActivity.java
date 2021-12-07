c3ab7f63bde6f3140c24866fdb4e661d8bab2332
dangthiminhngoc356 <81677658+dangthiminhngoc356@users.noreply.github.com>
2021-12-07 20:31:33 +0700

    home

--- app/src/main/java/com/example/appbantraicay/HomeActivity.java
+++ app/src/main/java/com/example/appbantraicay/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.appbantraicay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,8 @@ Button logout;
             @Override
             public void onClick(View view) {
                 finish();
+                Intent intent = new Intent(HomeActivity.this, Login.class);
+                startActivity(intent);
             }
         });
     }
